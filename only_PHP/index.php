a4d4b342dbda9ea77ae90435aab0da7a2c9dd57f
Luca <lu.cucchi@gmail.com>
2020-04-20 15:03:02 +0200

    (only PHP) html / sass in progress

---
+++ only_PHP/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="it" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="dist/css/style.css">
+        <title>Luca's Spotify</title>
+    </head>
+    <body>
+        <nav>
+            <img id="logo" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.freeiconspng.com%2Fuploads%2Fgreen-app-spotify-icon-0.png&f=1&nofb=1" alt="spotify-logo">
+            <h2>Luca's Spotify</h2>
+        </nav>
+    </body>
+</html>
