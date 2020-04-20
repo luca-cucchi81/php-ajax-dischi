54d03ec45c37d9bf9775588d075691f3c7cfbb80
Luca <lu.cucchi@gmail.com>
2020-04-20 17:05:41 +0200

    (with Ajax)  inserimento data.php + creazione json

---
+++ with_AJAX/data.php
@@ -0,0 +1,66 @@
+<?php
+    $db = [
+        [
+            'cover' => 'https://upload.wikimedia.org/wikipedia/en/6/68/RainbowRainbowRising.jpg',
+            'title' => 'Rising',
+            'artist' => 'Rainbow',
+            'year' => '1976'
+        ],
+        [
+            'cover' => 'https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png',
+            'title' => 'The Dark Side of the Moon',
+            'artist' => 'Pink Floyd',
+            'year' => '1973'
+        ],
+        [
+            'cover' => 'https://upload.wikimedia.org/wikipedia/en/2/20/Led_Zeppelin_-_Led_Zeppelin_II.jpg',
+            'title' => 'Led Zeppelin II',
+            'artist' => 'Led Zeppelin',
+            'year' => '1969'
+        ],
+        [
+            'cover' => 'https://upload.wikimedia.org/wikipedia/en/f/f8/JethroTullAPassionPlay.jpg',
+            'title' => 'A Passion Play',
+            'artist' => 'Jethro Tull',
+            'year' => '1973'
+        ],
+        [
+            'cover' => 'https://upload.wikimedia.org/wikipedia/en/4/49/Hotelcalifornia.jpg',
+            'title' => 'Hotel California',
+            'artist' => 'Eagles',
+            'year' => '1976'
+        ],
+        [
+            'cover' => 'https://upload.wikimedia.org/wikipedia/en/6/64/Black_Sabbath_-_Paranoid.jpg',
+            'title' => 'Paranoid',
+            'artist' => 'Black Sabbath',
+            'year' => '1970'
+        ],
+        [
+            'cover' => 'https://upload.wikimedia.org/wikipedia/en/5/54/Perfect_Strangers_%28Deep_Purple_album_-_cover_art%29.jpg',
+            'title' => 'Perfect Strangers',
+            'artist' => 'Deep Purple',
+            'year' => '1984'
+        ],
+        [
+            'cover' => 'https://upload.wikimedia.org/wikipedia/en/9/99/Eric_Clapton_Unplugged.jpg',
+            'title' => 'Unplugged',
+            'artist' => 'Eric Clapton',
+            'year' => '1992'
+        ],
+        [
+            'cover' => 'https://upload.wikimedia.org/wikipedia/en/c/cc/SRVinstep.jpg',
+            'title' => 'In Step',
+            'artist' => 'Stevie Ray Vaughan',
+            'year' => '1989'
+        ],
+        [
+            'cover' => 'https://upload.wikimedia.org/wikipedia/en/b/bd/Raised_on_Radio_%28Journey_album_-_cover_art%29.jpg',
+            'title' => 'Raised on Radio',
+            'artist' => 'Journey',
+            'year' => '1986'
+        ]
+
+    ]
+
+ ?>
